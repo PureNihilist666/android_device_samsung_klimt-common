8fbce5baf07076fd3de6d7d7292816492ba96bca
PureNihilist666 <pranav.nath@outlook.com>
2024-11-18 01:02:33 +0530

    klimt: Add framework compatibility matrix to fix vintf error
    Change-Id: Ifc8a80e40d29f84d0b91b756d5f8333c1820fcb3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -38,6 +38,9 @@ TARGET_SCREEN_DENSITY := 320
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
+# Compatibility Matrix
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
 # Legacy blobs support
 TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
     /system/vendor/bin/vcsFPService=22
